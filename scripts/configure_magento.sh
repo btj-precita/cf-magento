c7616f206ba4c3f9bef77eef3e1943fecae82c79
TanKhuu <jackiekhuu.work@gmail.com>
2018-01-24 14:45:26 +0700

    config stack for Demo Environment

--- scripts/configure_magento.sh
+++ scripts/configure_magento.sh
@@ -34,6 +34,39 @@ cd
 #fi
 [ -f "magento.tar.gz" ] && echo "Media file Found" || echo "Media file Not found"
 
+# Install PHP Composer
+sudo chown -R ec2-user:ec2-user /home/ec2-user/.composer
+sudo curl -s https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+# Configure deployer credential for git
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+# Clone precita project
+git clone https://git-codecommit.ap-southeast-1.amazonaws.com/v1/repos/precita
+cd precita/
+git checkout demo
+git pull
+
+# Codedeploy for single server script
+cat << EOF > ~/codedeploy
+#!/bin/bash
+# Simple Script for Magento Single Server Deployment #
+cd /home/ec2-user/precita/
+git pull
+git status
+read -n1 -r -p "Press any key to continue..." key
+cd /home/ec2-user/precita/app
+cp -R design /var/www/html/app/
+cp -R code /var/www/html/app/
+/var/www/html/bin/magento maintenance:enable
+/var/www/html/bin/magento setup:upgrade
+/var/www/html/bin/magento setup:di:compile
+/var/www/html/bin/magento setup:static-content:deploy vi_VN en_US
+/var/www/html/bin/magento maintenance:disable
+EOF
+chmod +x ~/codedeploy
+sudo mv ~/codedeploy /usr/local/bin/codedeploy
+
 cd /var/www/html
 tar xzf ~/magento.tar.gz
 
@@ -178,12 +211,6 @@ INSERT INTO core_config_data (scope, scope_id, path, value) VALUES ('default', 0
 EOF
 
 fi
-
-sudo chown -R ec2-user:ec2-user /home/ec2-user/.composer
-sudo curl -s https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-git config --global credential.helper '!aws codecommit credential-helper $@'
-git config --global credential.UseHttpPath true
 
 ./magento deploy:mode:set production
 
